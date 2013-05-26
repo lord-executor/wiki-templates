69f6696eeffd446360d6179dbe276a8251d082b2
Lukas Angerer <lord.of.war@gmx.ch>
2013-05-26 20:32:28 +0200

    - new branch for a more elaborate default template with   - syntax highlighting   - basic navigation   - icons (using font-awesome)

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,29 @@
 
+hljs.initHighlightingOnLoad();
+
 (function ($) {
 	$(document).ready(function () {
 		// set page title to first H1 content
 		$("title").text($("h1").first().text());
+		
+		var segments = location.pathname.split("/");
+		if (segments[segments.length - 1] == "")
+			segments.pop()
+		var crumbs = $.map(segments, function (value, index) {
+			var link = $("<a></a>").attr("href", segments.slice(0, index + 1).join("/"));
+			var item = $("<li></li>").append(link);
+			
+			if (index == 0) {
+				link.html("<i class=\"icon-home\"></i>");
+				link.attr("href", "/");
+			} else {
+				link.text(value);
+				item.prepend("<i class=\"separator icon-arrow-right\"></i>");
+			}
+			
+			
+			return item;
+		});
+		$("#breadcrumb ul").append(crumbs);
 	});
 })(jQuery);
