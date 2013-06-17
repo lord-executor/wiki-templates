47dc24572fc44f6dc2167f4fdd314549f33f57bc
Lukas Angerer <lukas.angerer@getunik.com>
2013-06-17 11:19:21 +0200

    - changing to wiki directory before launching the server

--- scripts/powershell/launch.ps1
+++ scripts/powershell/launch.ps1
@@ -3,4 +3,6 @@
 Start-Process "powershell" -NoNewWindow -ArgumentList "-Command `"& {start-sleep 1; Start-Process 'http://localhost:8888/'}`""
 
 $wikiDir = Resolve-Path (Join-Path (Split-Path -Parent $MyInvocation.MyCommand.Definition) "/../../..")
+
+Set-Location $wikiDir
 & $wikiDir/tools/minihttp/win/MiniHttp.exe --root=$wikiDir
